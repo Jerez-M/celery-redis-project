cf298178218a7fcd28f587da67e6c2a1b5081643
Jeremiah Muchazondida <jjmuchazondida@gmail.com>
2024-08-22 11:58:06 +0200

    Modified the swagger settings

--- backend_api/backend_api/urls.py
+++ backend_api/backend_api/urls.py
@@ -8,26 +8,34 @@ from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="GOAL INTERNATIONAL BACKEND API",
-        default_version='v1',
-        description="GOAL INTERNATIONAL DATABASE API",
+        title="Celery-Redis-Project",
+        default_version="v1",
+        description="Celery-Redis-Project",
         terms_of_service="",
         contact=openapi.Contact(email=""),
         license=openapi.License(name=""),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny, ],
-    authentication_classes=[]
+    permission_classes=[
+        permissions.AllowAny,
+    ],
+    authentication_classes=[],
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/v1/accounts/', include('accounts.urls')),
-    path('api/v1/organisations/', include('organisation.urls')),
-    path('api/v1/superusers/', include('superuser.urls')),
-    path('api/v1/administrators/', include('administrator.urls')),
-    path('api/v1/general-employees/', include('general_employee.urls'))
+    path("admin/", admin.site.urls),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/organisations/", include("organisation.urls")),
+    path("api/v1/superusers/", include("superuser.urls")),
+    path("api/v1/administrators/", include("administrator.urls")),
+    path("api/v1/general-employees/", include("general_employee.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
